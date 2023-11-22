d76c2170d4c30b5b34a4ca52979a62a61e453264
DarkGamer725 <darkgamer.725.oficial@gmail.com>
2023-11-22 20:45:35 +0100

    Arreglo menu Principal

--- Comercio/Comercio.py
+++ Comercio/Comercio.py
@@ -2,7 +2,7 @@ from menuCoche import menuCoches
 from menuAlquiler import menuAlquiler
 
 fin = False 
-while(fin == False):
+while(not fin):
     print("\n---- Menu Principal Comercio ----\n")
     print("Elige una opcion")
     print("1 - Menu Coches")
@@ -10,17 +10,13 @@ while(fin == False):
     print("0 - Salir")
     opcion = input("Elige una opcion")
     
-    if(opcion.isdigit()):
-        opcion = int(opcion)
-        if(opcion == 1):
-            menuCoches()
-        elif(opcion == 2):
-            menuAlquiler()
-        elif(opcion == 0):
-            fin = True
-            print("Fin de gestion del Comercio de alquileres de coches")
-        else:
-            print("Opcion no valida")
+    if(opcion == "1"):
+        menuCoches()
+    elif(opcion == "2"):
+        menuAlquiler()
+    elif(opcion == "0"):
+        fin = True
+        print("Fin de gestion del Comercio de alquileres de coches")
     else:
         print("Opcion no valida")
 
